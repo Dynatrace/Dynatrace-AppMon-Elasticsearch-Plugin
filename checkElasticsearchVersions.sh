22cc15a344f3534315d00b0981ab870df589ede1
Dominik Stadler <dominik.stadler@dynatrace.com>
2017-08-30 08:29:42 +0200

    Update verification tests to the latest versions of Elasticsearch, update some third-party libs

--- checkElasticsearchVersions.sh
+++ checkElasticsearchVersions.sh
@@ -2,8 +2,8 @@
 
 for i in test*.gradle;do
   ./gradlew clean && \
-  echo Checking with file $i && \
-  ./gradlew -b $i check
+  echo Checking with file ${i} && \
+  ./gradlew -b ${i} check
   if [ $? -ne 0 ];then
     exit 1
   fi
